a0cfe7d1e28f4fa5ae1da63210786b19ceb78973
布吉岛 <little3201@163.com>
2019-03-07 20:18:36 +0800

    添加一些配置和基础类

---
+++ src/main/java/top/abeille/common/controller/BasicController.java
@@ -0,0 +1,18 @@
+package top.abeille.common.controller;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * BasicController
+ *
+ * @author liwenqiang 2018/12/20 9:51
+ **/
+public abstract class BasicController {
+
+    /**
+     * 开启日志
+     */
+    protected static final Log log = LogFactory.getLog(BasicController.class);
+
+}
