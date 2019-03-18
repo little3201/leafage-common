3c022755d2610bf4b95011bf956a707c47c11180
little3201 <little3201@163.com>
2019-03-18 23:46:43 +0800

    修改类名

--- src/main/java/top/abeille/common/log/LogServerAspect.java
+++ src/main/java/top/abeille/common/log/LogServerAspect.java
@@ -24,32 +24,36 @@ public class LogServerAspect {
     }
 
     @Before("invokeAim()")
-    public void invokeBefore(JoinPoint joinPoint) throws Throwable {
+    public void invokeBefore(JoinPoint joinPoint) {
         startTime.set(System.currentTimeMillis());
         String className = joinPoint.getTarget().getClass().getName();
         String methodName = joinPoint.getSignature().getName();
-        // 省略日志记录内容
+        /*  */
     }
 
     @AfterReturning(returning = "ret", pointcut = "invokeAim()")
-    public void invokeAfterReturning(Object ret) throws Throwable {
-        // 处理完请求，返回内容
+    public void invokeAfterReturning(Object ret) {
+        /* 处理完请求，返回内容 */
         log.info("RESPONSE : " + ret);
         log.info("SPEND TIME : " + (System.currentTimeMillis() - startTime.get()));
     }
 
     @AfterThrowing(throwing = "ex", pointcut = "invokeAim()")
     public void invokeException(JoinPoint joinPoint, Throwable ex) {
-        String className = joinPoint.getTarget().getClass().getName(); //切入方法所属类名
-        String methodName = joinPoint.getSignature().getName(); //切入的方法名
-        Object[] params = joinPoint.getArgs(); //目标方法传入的参数
-        String param = "入参为：";
-
+        /* 切入方法所属类名 */
+        String className = joinPoint.getTarget().getClass().getName();
+        /* 切入方法名 */
+        String methodName = joinPoint.getSignature().getName();
+        /*  目标方法传入的参数 */
+        Object[] params = joinPoint.getArgs();
+        StringBuilder param = new StringBuilder();
         if (params != null && params.length > 0) {
+            param.append("入参为：");
             for (Object p : params) {
-                param += p + ",";
+                param.append(p).append(",");
             }
-            param = param.substring(0, param.lastIndexOf(","));
+            /* 去掉最后一个逗号 */
+            param.toString().substring(0, param.lastIndexOf(","));
         }
         log.error("[Exception]:[" + className + "]" + methodName + ":" + ex);
     }
