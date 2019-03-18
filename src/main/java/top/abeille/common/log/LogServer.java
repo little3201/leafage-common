63d75ba40781ca13e57f01d404580cb17178ca6c
little3201 <little3201@163.com>
2019-03-18 23:33:52 +0800

    修改切面

--- src/main/java/top/abeille/common/log/LogServer.java
+++ src/main/java/top/abeille/common/log/LogServer.java
@@ -1,7 +1,7 @@
 package top.abeille.common.log;
 
 /**
- * description
+ * 操作日志注解
  *
  * @author liwenqiang 2019/3/18 21:40
  **/
@@ -9,12 +9,12 @@ package top.abeille.common.log;
 import java.lang.annotation.*;
 
 /**
- * 自定义注解类
+ * 日志注解类
  */
 @Target(ElementType.METHOD) //注解放置的目标位置,METHOD是可注解在方法级别上
 @Retention(RetentionPolicy.RUNTIME) //注解在哪个阶段执行
 @Documented //生成文档
-public @interface AbeilleLog {
+public @interface LogServer {
 
     String value() default "";
 }
--- src/main/java/top/abeille/common/log/LogServerAspect.java
+++ src/main/java/top/abeille/common/log/LogServerAspect.java
@@ -7,39 +7,39 @@ import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
 /**
- * description
+ * 日志切面
  *
  * @author liwenqiang 2019/3/18 21:00
  **/
 @Aspect
 @Component
-public class ServerLogAspect {
+public class LogServerAspect {
 
-    protected static final Log log = LogFactory.getLog(ServerLogAspect.class);
+    protected static final Log log = LogFactory.getLog(LogServerAspect.class);
 
-    ThreadLocal<Long> startTime = new ThreadLocal<>();
+    private ThreadLocal<Long> startTime = new ThreadLocal<>();
 
-    @Pointcut("@annotation(top.abeille.common.log.AbeilleLog)")
-    public void webLog() {
+    @Pointcut("@annotation(top.abeille.common.log.LogServer)")
+    public void invokeAim() {
     }
 
-    @Before("webLog()")
-    public void doBefore(JoinPoint joinPoint) throws Throwable {
+    @Before("invokeAim()")
+    public void invokeBefore(JoinPoint joinPoint) throws Throwable {
         startTime.set(System.currentTimeMillis());
         String className = joinPoint.getTarget().getClass().getName();
         String methodName = joinPoint.getSignature().getName();
         // 省略日志记录内容
     }
 
-    @AfterReturning(returning = "ret", pointcut = "webLog()")
-    public void doAfterReturning(Object ret) throws Throwable {
+    @AfterReturning(returning = "ret", pointcut = "invokeAim()")
+    public void invokeAfterReturning(Object ret) throws Throwable {
         // 处理完请求，返回内容
         log.info("RESPONSE : " + ret);
         log.info("SPEND TIME : " + (System.currentTimeMillis() - startTime.get()));
     }
 
-    @AfterThrowing(throwing = "ex", pointcut = "webLog()")
-    public void exceptionDispose(JoinPoint joinPoint, Throwable ex) {
+    @AfterThrowing(throwing = "ex", pointcut = "invokeAim()")
+    public void invokeException(JoinPoint joinPoint, Throwable ex) {
         String className = joinPoint.getTarget().getClass().getName(); //切入方法所属类名
         String methodName = joinPoint.getSignature().getName(); //切入的方法名
         Object[] params = joinPoint.getArgs(); //目标方法传入的参数
@@ -52,7 +52,5 @@ public class ServerLogAspect {
             param = param.substring(0, param.lastIndexOf(","));
         }
         log.error("[Exception]:[" + className + "]" + methodName + ":" + ex);
-        System.out.println("【" + className + "】:" + methodName + "执行时出现异常：" + ex + "。");
-        System.out.println(param);
     }
 }
