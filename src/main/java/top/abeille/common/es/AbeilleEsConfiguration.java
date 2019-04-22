eeea966ea2b1766c2ac3d61c53c7c21c624ab280
李文强 <liwenqiang@tseverday.com>
2019-04-22 18:37:04 +0800

    修改日志model

--- src/main/java/top/abeille/common/es/AbeilleEsConfiguration.java
+++
@@ -1,9 +0,0 @@
-package top.abeille.common.es;
-
-/**
- * description
- *
- * @author liwenqiang 2019/4/12 8:35
- **/
-public class AbeilleEsConfiguration {
-}
--- src/main/java/top/abeille/common/log/model/LogInfoModel.java
+++ src/main/java/top/abeille/common/log/model/LogInfoModel.java
@@ -1,6 +1,8 @@
 package top.abeille.common.log.model;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
 
 /**
  * description
@@ -11,22 +13,86 @@ import javax.persistence.*;
 @Table(name = "log_info")
 public class LogInfoModel {
 
-    private Integer id;
-
     /**
-     * Get 主键
+     * 主键
      */
+    private Long id;
+    /**
+     * ip地址
+     */
+    private String ip;
+    /**
+     * 操作内容
+     */
+    private String operation;
+    /**
+     * 参数
+     */
+    private String param;
+    /**
+     * 操作用户ID
+     */
+    @NotNull
+    private Long modifierId;
+    /**
+     * 操作时间
+     */
+    @NotNull
+    private Date modifyTime;
+
     @Column(name = "id")
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    public Integer getId() {
+    public Long getId() {
         return id;
     }
 
-    /**
-     * Set 主键
-     */
-    public void setId(Integer id) {
+    public void setId(Long id) {
         this.id = id;
+    }
+
+    @Column(name = "ip")
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    @Column(name = "operation")
+    public String getOperation() {
+        return operation;
+    }
+
+    public void setOperation(String operation) {
+        this.operation = operation;
+    }
+
+    @Column(name = "param")
+    public String getParam() {
+        return param;
+    }
+
+    public void setParam(String param) {
+        this.param = param;
+    }
+
+    @Column(name = "modifier_id")
+    public Long getModifierId() {
+        return modifierId;
+    }
+
+    public void setModifierId(Long modifierId) {
+        this.modifierId = modifierId;
+    }
+
+    @Column(name = "modify_time")
+    public Date getModifyTime() {
+        return modifyTime;
+    }
+
+    public void setModifyTime(Date modifyTime) {
+        this.modifyTime = modifyTime;
     }
 }
