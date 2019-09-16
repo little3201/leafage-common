1e156aad44e6c3f2906e8fa86bb2e95fe08ca254
李文强 <liwenqiang@tseverday.com>
2019-09-16 11:34:52 +0800

    修改测试

--- src/main/java/top/abeille/common/mock/AbstractControllerMock.java
+++ src/main/java/top/abeille/common/mock/AbstractControllerMock.java
@@ -3,7 +3,6 @@
  */
 package top.abeille.common.mock;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.mockito.MockitoAnnotations;
@@ -11,12 +10,10 @@ import org.mockito.junit.MockitoJUnitRunner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.util.MultiValueMap;
+import org.springframework.http.HttpHeaders;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.util.function.Consumer;
 
 /**
  * Controller Parent
@@ -32,8 +29,7 @@ public abstract class AbstractControllerMock<T> {
      */
     private static final Logger log = LoggerFactory.getLogger(AbstractControllerMock.class);
 
-    private final ObjectMapper objectMapper = new ObjectMapper();
-    private MockMvc mockMvc;
+    private WebTestClient webTestClient;
 
     /**
      * 设置要测试的controller
@@ -42,62 +38,38 @@ public abstract class AbstractControllerMock<T> {
      */
     protected abstract T getController();
 
-    public MockMvc getMockMvc() {
-        return mockMvc;
-    }
-
     @Before
     public void setupMock() {
         /* initialize mock object */
-        this.mockMvc = MockMvcBuilders.standaloneSetup(this.getController()).build();
+        this.webTestClient = WebTestClient.bindToController(getController()).build();
         MockitoAnnotations.initMocks(this);
     }
 
     /**
-     * ====================  POST 重载 添加参数到request body ====================
+     * ====================  POST ====================
      */
-    public MvcResult postTest(String url, Object obj) throws Exception {
-        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.post(url).content(objectMapper.writeValueAsString(obj)));
-        return resultActions.andReturn();
+    public WebTestClient.ResponseSpec postTest(String url, Object obj) {
+        return webTestClient.post().uri(url).syncBody(obj).exchange();
     }
 
     /**
-     * ====================  POST 重载 添加Map类型参数到请求中 ====================
+     * ====================  GET ====================
      */
-    public MvcResult postTest(String url, MultiValueMap<String, String> params) throws Exception {
-        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.post(url));
-        return resultActions.andReturn();
-    }
-
-    /**
-     * ====================  GET 重载 添加参数到request body ====================
-     */
-    public MvcResult getTest(String url, Object obj) throws Exception {
-        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.get(url).content(objectMapper.writeValueAsString(obj)));
-        return resultActions.andReturn();
-    }
-
-    /**
-     * ====================  GET 重载 添加Map类型参数到请求中 ====================
-     */
-    public MvcResult getTest(String url, MultiValueMap<String, String> params) throws Exception {
-        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.get(url));
-        return resultActions.andReturn();
+    public WebTestClient.ResponseSpec getTest(String url, Consumer<HttpHeaders> params) {
+        return webTestClient.get().uri(url).headers(params).exchange();
     }
 
     /**
      * ====================  PUT  ====================
      */
-    public MvcResult putTest(String url, Object obj) throws Exception {
-        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.put(url).content(objectMapper.writeValueAsString(obj)));
-        return resultActions.andReturn();
+    public WebTestClient.ResponseSpec putTest(String url, Object obj) {
+        return webTestClient.put().uri(url).syncBody(obj).exchange();
     }
 
     /**
      * ====================  DELETE  ====================
      */
-    public MvcResult deleteTest(String url, Object obj) throws Exception {
-        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.delete(url).content(objectMapper.writeValueAsString(obj)));
-        return resultActions.andReturn();
+    public WebTestClient.ResponseSpec deleteTest(String url, Consumer<HttpHeaders> params) throws Exception {
+        return webTestClient.delete().uri(url).headers(params).exchange();
     }
 }
