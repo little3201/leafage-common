e7bc3195640e4fb09c2564a901aaaa3ee5c6ecb5
little3201 <little3201@163.com>
2020-02-26 18:11:41 +0800

    source修改为resource； 生成id的方法修改

--- src/main/java/top/abeille/common/basic/AbstractBasicService.java
+++ src/main/java/top/abeille/common/basic/AbstractBasicService.java
@@ -5,16 +5,41 @@
 package top.abeille.common.basic;
 
 import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class AbstractBasicService {
 
     /**
-     * 获取当前日期的年月日，拼接为字符串
+     * 获取当前日期的月份和日期，将月份转换为十六进制
      *
      * @return 拼接的日期，例如：20200213
      */
-    protected String getDateValue() {
+    protected String generateId() {
         LocalDate localDate = LocalDate.now();
-        return String.valueOf(localDate.getYear()) + localDate.getMonthValue() + localDate.getDayOfMonth();
+        String month = Integer.toHexString(localDate.getMonthValue()).toUpperCase();
+        int day = localDate.getDayOfMonth();
+        generateRandom();
+        return month + day + generateRandom();
+    }
+
+    /**
+     * 生成四位随机码
+     *
+     * @return 拼接的日期，例如：20200213
+     */
+    private String generateRandom() {
+        String[] beforeShuffle = new String[]{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+                "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P",
+                "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+        List<String> list = Arrays.asList(beforeShuffle);
+        Collections.shuffle(list);
+        StringBuilder sb = new StringBuilder();
+        for (Object o : list) {
+            sb.append(o);
+        }
+        String afterShuffle = sb.toString();
+        return afterShuffle.substring(5, 9);
     }
 }
