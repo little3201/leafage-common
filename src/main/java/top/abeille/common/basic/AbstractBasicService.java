89b696586ddf8c5b2e532186d93daa4a99420441
李文强 <liwenqiang@tseverday.com>
2020-03-02 16:12:18 +0800

    修改id获取的集合

--- src/main/java/top/abeille/common/basic/AbstractBasicService.java
+++ src/main/java/top/abeille/common/basic/AbstractBasicService.java
@@ -11,6 +11,10 @@ import java.util.List;
 
 public class AbstractBasicService {
 
+    private static List<String> list = Arrays.asList("0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+            "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P",
+            "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
+
     /**
      * 获取当前日期的月份和日期，将月份转换为十六进制，拼接日期和四位随机码
      *
@@ -28,16 +32,11 @@ public class AbstractBasicService {
      * @return 组合的四位码，例如：03JA
      */
     private String generateRandom() {
-        String[] beforeShuffle = new String[]{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
-                "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P",
-                "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
-        List<String> list = Arrays.asList(beforeShuffle);
         Collections.shuffle(list);
         StringBuilder sb = new StringBuilder();
         for (Object o : list) {
             sb.append(o);
         }
-        String afterShuffle = sb.toString();
-        return afterShuffle.substring(5, 9);
+        return sb.toString().substring(5, 9);
     }
 }
