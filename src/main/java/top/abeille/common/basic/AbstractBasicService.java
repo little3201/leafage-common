c0d8c3b57bd59ab46d6a802e1569a7a237174a5a
little3201 <little3201@163.com>
2020-03-12 08:44:41 +0800

    修改AbstractBasicService为抽象类

--- src/main/java/top/abeille/common/basic/AbstractBasicService.java
+++ src/main/java/top/abeille/common/basic/AbstractBasicService.java
@@ -8,7 +8,7 @@ import java.time.LocalDate;
 import java.util.Collections;
 import java.util.List;
 
-public class AbstractBasicService {
+public abstract class AbstractBasicService {
 
     private static List<String> list = List.of("0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
             "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P",
@@ -17,7 +17,7 @@ public class AbstractBasicService {
     /**
      * 获取当前日期的月份和日期，将月份转换为十六进制，拼接日期和四位随机码
      *
-     * @return 拼接的日期，例如：A2103JA
+     * @return 拼接的编号，例如：A2103JA
      */
     protected String generateId() {
         LocalDate localDate = LocalDate.now();
