cfda2474f27ee564e78d31961022af476fb8d6f6
李文强 <liwenqiang@tseverday.com>
2020-03-30 19:36:33 +0800

    添加create方法，ID生成方法

---
+++ src/main/java/top/abeille/common/basic/AbstractBasicService.java
@@ -0,0 +1,37 @@
+package top.abeille.common.basic;
+
+import java.time.LocalDate;
+import java.util.Collections;
+import java.util.List;
+
+public abstract class AbstractBasicService {
+
+    private static List<String> list = List.of("0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+            "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P",
+            "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
+
+    /**
+     * 获取当前日期的月份和日期，将月份转换为十六进制，拼接日期和四位随机码
+     *
+     * @return 拼接的编号，例如：A2103JA
+     */
+    protected String generateId() {
+        LocalDate localDate = LocalDate.now();
+        // 月份转换为十六进制
+        return Integer.toHexString(localDate.getMonthValue()).toUpperCase() + localDate.getDayOfMonth() + generateRandom();
+    }
+
+    /**
+     * 生成四位随机码
+     *
+     * @return 组合的四位码，例如：03JA
+     */
+    private String generateRandom() {
+        Collections.shuffle(list);
+        StringBuilder sb = new StringBuilder();
+        for (String s : list) {
+            sb.append(s);
+        }
+        return sb.toString().substring(5, 9);
+    }
+}
