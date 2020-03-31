758465a682a2f9e9b46d31200012ada6059c950a
李文强 <liwenqiang@tseverday.com>
2020-03-31 14:42:50 +0800

    common中生成随机数的List.of改为Arrays.asList,List.of不可修改，使用报错

--- src/main/java/top/abeille/common/basic/AbstractBasicService.java
+++ src/main/java/top/abeille/common/basic/AbstractBasicService.java
@@ -4,13 +4,15 @@
 
 package top.abeille.common.basic;
 
+import java.security.SecureRandom;
 import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public abstract class AbstractBasicService {
 
-    private static List<String> list = List.of("0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+    private static List<String> list = Arrays.asList("0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
             "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P",
             "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
 
