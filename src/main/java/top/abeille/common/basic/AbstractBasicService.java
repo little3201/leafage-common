49f59639f2c96cb345b99207eaab2ee682171faf
李文强 <liwenqiang@tseverday.com>
2020-03-31 14:11:05 +0800

    修改随机字符获取的逻辑

--- src/main/java/top/abeille/common/basic/AbstractBasicService.java
+++ src/main/java/top/abeille/common/basic/AbstractBasicService.java
@@ -1,5 +1,6 @@
 package top.abeille.common.basic;
 
+import java.security.SecureRandom;
 import java.time.LocalDate;
 import java.util.Collections;
 import java.util.List;
@@ -29,9 +30,10 @@ public abstract class AbstractBasicService {
     private String generateRandom() {
         Collections.shuffle(list);
         StringBuilder sb = new StringBuilder();
-        for (String s : list) {
-            sb.append(s);
+        for (int i = 0; i < 4; i++) {
+            int nextInt = new SecureRandom().nextInt(list.size());
+            sb.append(list.get(nextInt));
         }
-        return sb.toString().substring(5, 9);
+        return sb.toString();
     }
 }
