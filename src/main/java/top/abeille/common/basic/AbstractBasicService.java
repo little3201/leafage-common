a67b84a178b37fb61baa238c19d5b7c8e5427c54
李文强 <liwenqiang@tseverday.com>
2020-06-24 16:14:13 +0800

    assets 的prod 环境 mongo配置修改

--- src/main/java/top/abeille/common/basic/AbstractBasicService.java
+++ src/main/java/top/abeille/common/basic/AbstractBasicService.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 public abstract class AbstractBasicService {
 
-    private static List<String> list = Arrays.asList("0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+    private static final List<String> list = Arrays.asList("0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
             "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P",
             "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
 
--- src/main/java/top/abeille/common/utils/FileBasicUtils.java
+++ src/main/java/top/abeille/common/utils/FileBasicUtils.java
@@ -24,28 +24,50 @@ public class FileBasicUtils {
      * 根据路径，后缀，获取指定路径下的文件
      *
      * @param filePath 读取路径
-     * @param suffix   文件后缀，可传入多类型，使用逗号分隔
+     * @param suffix   文件后缀，可传入多类型，逗号分隔
      * @return List<File>
      */
-    public static List<File> readWantedFilesUnderPath(String filePath, String suffix) {
-        List<File> list = new ArrayList<>();
+    public static List<File> readSuffixFilesUnderPath(String filePath, String suffix) {
         File curPath = new File(filePath);
         File[] files = curPath.listFiles();
         if (files == null || files.length == 0) {
             log.info("该路径: {}下没有任何文件", filePath);
             return Collections.emptyList();
         }
-        String[] suffixes = suffix.split(",");
-        for (String curSuffix : suffixes) {
-            log.info("获取后缀为：{} 的目标文件", "." + curSuffix);
-            for (File file : files) {
-                if (file.getName().endsWith("." + curSuffix)) {
-                    list.add(file);
+        List<File> fileList = new ArrayList<>();
+        for (File file : files) {
+            if (file.isFile()) {
+                String[] suffixes = suffix.split(",");
+                for (String curSuffix : suffixes) {
+                    log.info("获取后缀为：{} 的目标文件", "." + curSuffix);
+                    if (file.getName().endsWith("." + curSuffix.toUpperCase()) || file.getName().endsWith("." + curSuffix.toLowerCase())) {
+                        fileList.add(file);
+                    }
                 }
+            } else if (file.isDirectory()) {
+                List<File> list = readSuffixFilesUnderPath(file.getPath(), suffix);
+                fileList.addAll(list);
             }
-            log.info("获取到的目标文件数量为：{}", list.size());
         }
-        return list;
+        return fileList;
     }
 
+    /**
+     * 获取字符串中第一个指定字符类型的位置
+     * <p>
+     * eg: 获取第一个中文的位置, splitFirstScript("001目标", Character.UnicodeScript.HAN), return 3
+     *
+     * @param splitStr 要截取的字符串
+     * @return 一个指定字符类型在字符串中的位置
+     */
+    private static int splitFirstScript(String splitStr, Character.UnicodeScript unicodeScript) {
+        char[] chars = splitStr.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            Character.UnicodeScript sc = Character.UnicodeScript.of(chars[i]);
+            if (sc == unicodeScript) {
+                return i;
+            }
+        }
+        return 0;
+    }
 }
