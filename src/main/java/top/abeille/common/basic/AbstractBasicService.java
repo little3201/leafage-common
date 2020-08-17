ac52354e46802528680732939b8ce8f812165f24
little3201 <little3201@163.com>
2020-08-18 07:38:54 +0800

    common: 添加获取每周开始、结束日期；每月开始结束日期； assets: 添加获取当月每周新增记录数统计；添加每月访问量top10的文章；

--- src/main/java/top/abeille/common/basic/AbstractBasicService.java
+++ src/main/java/top/abeille/common/basic/AbstractBasicService.java
@@ -5,7 +5,9 @@
 package top.abeille.common.basic;
 
 import java.security.SecureRandom;
+import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.Month;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -41,4 +43,67 @@ public abstract class AbstractBasicService {
         }
         return sb.toString();
     }
+
+    /**
+     * 获取指定日期所在周的最后一天
+     *
+     * @param localDate 指定日期
+     * @return 最后一天的日期
+     */
+    protected static LocalDate lastDayOfWeek(LocalDate localDate) {
+        if (null == localDate) {
+            localDate = LocalDate.now();
+        }
+        DayOfWeek week = localDate.getDayOfWeek();
+        if (DayOfWeek.SATURDAY.equals(week)) {
+            return localDate;
+        }
+        return LocalDate.from(localDate.plusDays(7 - week.getValue() - 1));
+    }
+
+    /**
+     * 获取指定日期所在周的第一天
+     *
+     * @param localDate 指定日期
+     * @return 第一天的日期
+     */
+    protected LocalDate firstDayOfWeek(LocalDate localDate) {
+        if (null == localDate) {
+            localDate = LocalDate.now();
+        }
+        DayOfWeek week = localDate.getDayOfWeek();
+        if (DayOfWeek.SUNDAY.equals(week)) {
+            return localDate;
+        }
+        return LocalDate.from(localDate.minusDays(7 - (7 - week.getValue())));
+    }
+
+    /**
+     * 获取指定日期所在月的第一天
+     *
+     * @param localDate 指定日期
+     * @return 第一天的日期
+     */
+    protected LocalDate firstDayOfMonth(LocalDate localDate) {
+        if (null == localDate) {
+            localDate = LocalDate.now();
+        }
+        Month month = localDate.getMonth();
+        return LocalDate.of(localDate.getYear(), month, 1);
+    }
+
+    /**
+     * 获取指定日期所在月的最后一天
+     *
+     * @param localDate 指定日期
+     * @return 最后一天的日期
+     */
+    protected LocalDate lastDayOfMonth(LocalDate localDate) {
+        if (null == localDate) {
+            localDate = LocalDate.now();
+        }
+        Month month = localDate.getMonth();
+        return LocalDate.of(localDate.getYear(), month, month.length(localDate.isLeapYear()));
+    }
+
 }
