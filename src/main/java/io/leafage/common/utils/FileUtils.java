f1c03560735ee38f87eebfcd54a6b6abb62bcb1c
little3201 <little3201@163.com>
2021-04-15 16:36:07 +0800

    删除封装的test

--- src/main/java/io/leafage/common/utils/FileUtils.java
+++ src/main/java/io/leafage/common/utils/FileUtils.java
@@ -3,9 +3,6 @@
  */
 package io.leafage.common.utils;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -16,11 +13,9 @@ import java.util.List;
  *
  * @author liwenqiang 2019/3/25 14:43
  **/
-public class FileBasicUtils {
+public class FileUtils {
 
-    protected static final Logger log = LoggerFactory.getLogger(FileBasicUtils.class);
-
-    private FileBasicUtils() {
+    private FileUtils() {
     }
 
     /**
@@ -30,23 +25,20 @@ public class FileBasicUtils {
      * @param suffix   文件后缀，可传入多类型，使用逗号分隔
      * @return List<File>
      */
-    public static List<File> readWantedFilesUnderPath(String filePath, String suffix) {
+    public static List<File> read(String filePath, String suffix) {
         List<File> list = new ArrayList<>();
         File curPath = new File(filePath);
         File[] files = curPath.listFiles();
         if (files == null || files.length == 0) {
-            log.info("该路径: {}下没有任何文件", filePath);
             return Collections.emptyList();
         }
         String[] suffixes = suffix.split(",");
         for (String curSuffix : suffixes) {
-            log.info("获取后缀为：{} 的目标文件", curSuffix);
             for (File file : files) {
                 if (file.getName().endsWith("." + curSuffix)) {
                     list.add(file);
                 }
             }
-            log.info("获取到的目标文件数量为：{}", list.size());
         }
         return list;
     }
